cac471050c385a03ea0a192e4fdfae324fda222c
sudhakard-cloudside <87324686+sudhakar678@users.noreply.github.com>
2022-05-24 16:42:26 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,3 +2,13 @@ variable "fuction_name" {
   type = string 
   defaultv= "welcome"
 }
+
+variable "hanlder" {
+  type = string 
+  defaultv= "welcome.hello"
+}
+
+variable "runtime" {
+  type = string 
+  defaultv= "python 3.8"
+}
